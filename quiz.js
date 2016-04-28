37d92c6d2ee8e9383089d4746720fe3a75d9ac59
utleroy <bryanhankins@me.com>
2016-04-27 19:50:50 -0500

    adding and committing so I can delete an unused branch

--- quiz.js
+++ quiz.js
@@ -6,8 +6,10 @@ function populatePage (inventory) { // this is the inventory from the callback o
 	for(var i = 0; i < inventory.length; i++) {
 		let currentCar = inventory[i];
       output += `<div class="col-xs-3"><h1>${currentCar.make} ${currentCar.model}</h1><p>${currentCar.model}</p><p>${currentCar.year}</p><p>${currentCar.price}</p><p>${currentCar.color}</p><p>${currentCar.purchased}</p><p>${currentCar.description}</p></div>`;
+      // if (currentCar.color === "red") {
+      //   return document.getElementsByClassName("col-xs-3").style.border = "red";
+      // }
 
-      //-->use bootstrap grid system to layout columns<--//
 	displayCar.innerHTML = output;
 		
 	}
@@ -17,10 +19,6 @@ function populatePage (inventory) { // this is the inventory from the callback o
 
 CarLot.loadInventory(populatePage);
 
-  // Loop over the inventory and populate the page
 
   // Now that the DOM is loaded, establish all the event listeners needed
   // CarLot.activateEvents();
-
-// Load the inventory and send a callback function to be
-// invoked after the process is complete
